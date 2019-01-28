588f67673a81448f14ac95614296b07e119ffb27
Julien AZOUZ <juazouz@e3r4p7.42.fr>
2019-01-28 13:24:09 +0100

    Core structures

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 13:32:25 by juazouz           #+#    #+#             */
-/*   Updated: 2019/01/28 12:51:48 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/01/28 13:22:53 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,32 @@
 /*
 **	Types.
 */
+
+typedef	struct s_lem_in	t_lem_in;
+typedef	struct s_room	t_room;
+typedef	struct s_point	t_point;
+
+struct	s_lem_in
+{
+	int		total_ants;
+	t_room	*start;
+	t_room	*end;
+};
+
+struct	s_room
+{
+	char	*name;
+	t_point	pos;
+	int		ants;
+	t_list	*links;
+};
+
+struct s_point
+{
+	int	x;
+	int	y;
+};
+
 
 /*
 **	Core.
