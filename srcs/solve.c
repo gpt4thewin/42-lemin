16bcb7d9c33a53e5e2658147aaa912719b32c390
Adam GOULAS <agoulas@e3r5p10.42.fr>
2019-01-29 19:34:30 +0100

    modif model et ant_can_move add

--- srcs/solve.c
+++ srcs/solve.c
@@ -6,14 +6,37 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 18:33:02 by juazouz           #+#    #+#             */
-/*   Updated: 2019/01/28 19:03:41 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/01/29 19:28:10 by agoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+/*
+**	Function testing the links for if the next room is empty
+*/
+
+t_bool		ants_cam_move(t_list *links)
+{
+	t_room *r;
+
+	r = links->content;
+	if ((r->ants == 0 && r->type == standard) || (r->type == end))
+		return (1);
+	return (0);
+}
+
+/*
+** Function of resolution of the programme
+*/
+
 void	solve(t_lem_in *lem_in, t_solution *solution)
 {
-	(void)lem_in;
-	(void)solution;
+	ft_printf("test solve\n");
+	return ;
+	while (lem_in->end->ants != lem_in->total_ants)
+	{
+		solution_add_round(solution);
+		
+	}
 }
