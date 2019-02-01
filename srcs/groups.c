498735ce71e0122c368b12e0f9cc5619d4ef7cdd
Adam GOULAS <agoulas@e3r4p3.42.fr>
2019-02-01 16:45:31 +0100

    modification of the groups.c

--- srcs/groups.c
+++ srcs/groups.c
@@ -6,17 +6,58 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 19:27:06 by agoulas           #+#    #+#             */
-/*   Updated: 2019/01/31 19:23:32 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/02/01 16:29:07 by agoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
 /*
+** Function to test is group_a is equal to group_b
+*/
+
+t_bool	groups_equals(t_group *group_a, t_group *group_b)
+{
+	t_glist *routes_a;
+	t_glist *routes_b;
+
+	if (group_a->count == group_b->count
+		&& group_a->high_len == group_b->high_len
+		&& group_a->low_len == group_b->low_len)
+	{
+		routes_a = group_a->routes;
+		routes_b = group_b->routes;
+		if (routes_equals(routes_a, routes_b) == group_a->count)
+			return (1);
+		return (0);
+	}
+	else
+		return (false);
+}
+
+/*
+** Function testing if "group" has a doublon in the list
+*/
+
+t_bool	groups_doublon_group(t_glist *groups, t_group *group)
+{
+	t_glist *groups_b;
+
+	groups_b = groups;
+	if (groups_b)
+	{
+		if (groups_equals(group, groups_b->content) == 1)
+			return (true);
+		groups_b = groups_b->next;
+	}
+	return (false);
+}
+
+/*
 **	Function to add a group to the list of groups
 */
 
-void	groups_add_group(t_glist **groups, t_group **group)
+void	groups_add_group(t_glist **groups, t_group *group)
 {
 	if (groups && group)
 		ft_glstadd(groups, ft_glstnew(group, sizeof(group)));
@@ -33,35 +74,7 @@ void	build_groups(t_glist **groups, t_glist *routes)
 	curr_route = routes;
 	while (curr_route != NULL)
 	{
-		group_route_conflict(groups, routes, curr_route->route);
+		group_route_conflict(groups, (curr_route)->route, routes);
 		curr_route = curr_route->next;
-	}
-}
-
-/*
-**	Function to retest the group and concatene les group
-**	et limiting the number of group
-*/
-
-void	group_organise(t_group **group)
-{
-	t_glist		*curr_routes;
-	t_glist		*ind_route;
-	t_route		tmp;
-
-
-	curr_routes = (*group)->routes;
-	while (curr_routes != NULL
-			&& curr_routes->id_route != (*group)->id)
-	{
-		tmp = curr_routes->route;
-		ind_route = (*group)->routes;
-		while (ind_route != NULL && ind_route->next != NULL)
-		{
-			if (route_cmp_conflit(tmp, ind_route->route) == 0)
-
-			ind_route = ind_route->next;
-		}
-		curr_routes = curr_routes->next;
 	}
 }
