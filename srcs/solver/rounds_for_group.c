37763fdbc248079e2457cda60b9fea496dbab00f
Julien AZOUZ <juazouz@e1r2p23.42.fr>
2019-02-06 15:17:14 +0100

    solver: fix rounds_for_group() prototype

--- srcs/solver/rounds_for_group.c
+++ srcs/solver/rounds_for_group.c
@@ -6,7 +6,7 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 18:38:13 by juazouz           #+#    #+#             */
-/*   Updated: 2019/02/05 19:30:11 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/02/06 14:56:08 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@
 **	using the specified group.
 */
 
-int	group_rounds_needed(t_group *group, int total_ants)
+int	group_total_rounds(t_group *group, int total_ants)
 {
 	(void)group;
 	(void)total_ants;
+
+	return (0);
 }
