56faa566f4264d3dc7316935b7ef00e0a9c46af3
Julien AZOUZ <juazouz@e2r3p16.42.fr>
2019-02-06 18:36:01 +0100

    group_creator: add conflict bitmap allocation

--- srcs/group_creator/create_groups.c
+++ srcs/group_creator/create_groups.c
@@ -6,7 +6,7 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 19:27:06 by agoulas           #+#    #+#             */
-/*   Updated: 2019/02/05 19:39:31 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/02/06 17:58:05 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 ** Function testing if "group" has a doublon in the list
 */
 
-t_bool	groups_has_duplicate(t_glist *groups, t_group *group)
+t_bool		groups_has_duplicate(t_glist *groups, t_group *group)
 {
 	t_glist *groups_b;
 
@@ -34,19 +34,34 @@ t_bool	groups_has_duplicate(t_glist *groups, t_group *group)
 **	Function to add a group to the list of groups
 */
 
-void	groups_add(t_glist **groups, t_group *group)
+void		groups_add(t_glist **groups, t_group *group)
 {
 	ft_glstadd(groups, ft_glstnew(group, sizeof(group)));
+}
+
+static void	allocate_conflicts_bitmap(t_glist *routes)
+{
+	int		len;
+	t_glist	*curr;
+
+	len = ft_glstlen(routes);
+	curr = routes;
+	while (curr != NULL)
+	{
+		curr->route->conflicts = bitmap_new(len);
+		curr = curr->next;
+	}
 }
 
 /*
 **	Function to create all groups  from the list of routes
 */
 
-void	create_groups(t_glist **groups, t_glist *routes)
+void		create_groups(t_glist **groups, t_glist *routes)
 {
 	t_glist *curr_route;
 
+	allocate_conflicts_bitmap(routes);
 	curr_route = routes;
 	while (curr_route != NULL)
 	{
