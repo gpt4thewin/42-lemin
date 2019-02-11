5f8db1a9fcf0ff168a7520f0911501ec85f0ff8c
Adam GOULAS <agoulas@e2r4p12.42.fr>
2019-02-11 17:00:10 +0100

    cretion of function of build_solution and modifcation of parameter in other function

--- srcs/solver/build_solution.c
+++ srcs/solver/build_solution.c
@@ -12,18 +12,60 @@
 
 #include "lem_in.h"
 
+
+/*
+**	Move all ants of one route of the group.
+*/
+
+static void	ants_of_node_route(t_solution *solution, t_lem_in *lem_in, t_route *route)
+{
+	t_glist *curr;
+	t_room *room_a;
+	t_room *room_b;
+
+	curr = route->rooms;
+	while(curr->room != lem_in->start)
+	{
+		room_a = curr->room;
+		room_b = curr->next->room;
+		if (curr->room->type != end)
+			move_ants(&room_b, &room_a, solution);
+		if (room_b->type == start)
+			break ;
+		ft_printf(" ");
+		curr = curr->next;
+	}
+}
+
+/*
+** test if all the group's route ofr moving ants.
+*/
+
+static void	ants_of_node(t_solution *solution, t_group *group, t_lem_in *lem_in)
+{
+	t_glist *curr;
+
+	curr = group->routes;
+	while(curr != NULL)
+	{
+		ants_of_node_route(solution, lem_in, curr->route);
+		curr = curr->next;
+	}
+}
+
 /*
 **	Builds a solution or a set of moves using the specified group.
 */
 
-void	build_solution(t_solution *solution, t_group *group, int total_ants)
+void		build_solution(t_solution *solution, t_group *group, t_lem_in *lem_in)
 {
-	(void)solution;
-	(void)group;
-	(void)total_ants;
-	// int	ants_left;
-	// int	*maps;
+	int ants_left;
 
-	// maps = ft_memalloc(group->count * sizeof(int*));
-
+	while ((lem_in->start->ants) >= 0
+		&& lem_in->start->ants != lem_in->total_ants)
+	{
+		ants_left = lem_in->total_ants - lem_in->end->ants;
+		ants_of_node(solution, group, lem_in);
+		solution_add_round(solution);
+	}
 }
--- srcs/solver/solve.c
+++ srcs/solver/solve.c
@@ -6,7 +6,7 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 18:33:02 by juazouz           #+#    #+#             */
-/*   Updated: 2019/02/06 14:59:32 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/02/11 14:13:35 by agoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ void	solve(t_lem_in *lem_in, t_solution *solution)
 
 	groups = NULL;
 	routes = create_routes(lem_in);
-	create_groups(&groups, routes);
+	create_groups(&groups, routes, lem_in);
 	best_group = select_best_group(groups);
 	sort_routes(best_group);
-	build_solution(solution, best_group, lem_in->total_ants);
+	build_solution(solution, best_group, lem_in);
 	ft_glstdel(&routes, route_free);
 	ft_glstdel(&groups, group_free);
 }
