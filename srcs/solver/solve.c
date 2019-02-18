9d6b933c0867f2b4d4ab0d6508ee7acc659e8a46
Adam GOULAS <agoulas@e1r4p12.42.fr>
2019-02-18 18:10:23 +0100

    modif static function

--- srcs/solver/solve.c
+++ srcs/solver/solve.c
@@ -17,8 +17,7 @@
 **	Selects the best group and prints the solution.
 */
 
-static void	print_debug(t_lem_in *lem_in, t_solution *solution,
-	t_group *best_group, t_bool debug)
+static void	print_debug(t_lem_in *lem_in, t_group *best_group, t_bool debug)
 {
 	if (debug == true)
 	{
@@ -44,7 +43,7 @@ void		solve(t_lem_in *lem_in, t_solution *solution)
 	routes = create_routes(lem_in);
 	create_groups(&groups, routes, lem_in);
 	best_group = select_best_group(groups, lem_in->total_ants);
-	debug(lem_in, solution, best_group, false);
+	print_debug(lem_in, best_group, false);
 	build_solution(lem_in, best_group, solution);
 	ft_glstdel(&routes, route_free);
 	ft_glstdel(&groups, group_free);
