87ea448b33ace38d7f776e9d3caf4c27f32b9ef1
Adam GOULAS <agoulas@e2r4p14.42.fr>
2019-02-22 15:50:36 +0100

    Optimization parse_links

--- srcs/parser/parse_core.c
+++ srcs/parser/parse_core.c
@@ -42,7 +42,8 @@ void	parse_ants_count(t_lem_in *lem_in, char **line)
 int		read_room_line(t_lem_in *lem_in, char **line, t_roomtype type)
 {
 	save_line(lem_in, *line);
-	if (gnl_no_comm(0, line) == -1)
+	if (gnl_no_comm(0, line) == -1
+		|| (ft_strncmp(*line, "L", 1) == true))
 		lem_in_die();
 	parse_room(lem_in, *line, type);
 	return (1);
