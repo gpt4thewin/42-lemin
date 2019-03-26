9d43bee7a6d12aaa50f5569ed86f20146f77a6bf
Julien AZOUZ <juazouz@e1r8p9.42.fr>
2019-03-26 14:16:48 +0100

    correctif enregistrement de l'entree

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/07 09:59:35 by agoulas           #+#    #+#             */
-/*   Updated: 2019/03/19 12:57:10 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/03/26 14:11:14 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,17 +68,20 @@ int		gnl_no_comm(t_lem_in *lem_in, const int fd, char **line)
 
 	res = 0;
 	ft_strdel(line);
-	while ((res = get_next_line(fd, line)) >= 0
-			&& ft_strnequ("#", *line, 1)
-			&& !(ft_strequ("##start", *line))
-			&& !(ft_strequ("##end", *line)))
+	while (1)
 	{
+		res = get_next_line(fd, line);
+		if (res <= 0)
+		{
+			return (res);
+		}
 		save_output(lem_in, *line);
+		if (!ft_strnequ("#", *line, 1)
+			|| (ft_strequ("##start", *line))
+			|| (ft_strequ("##end", *line)))
+			return (res);
 		ft_strdel(line);
 	}
-	if (res == -1)
-		return (-1);
-	return (res);
 }
 
 /*
